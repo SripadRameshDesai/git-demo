6c5ec6abac14ff330728fc7e5cafd30555fa4d08
SripadRameshDesai <shripadrameshdesai@gmail.com>
2021-08-30 16:41:38 +0530

    added git-demo project to local repo

---
+++ src/main/java/com/javatechie/app/gitdemo/App.java
@@ -0,0 +1,14 @@
+package com.javatechie.app.gitdemo;
+
+public class App {
+
+    public void addData(String data){
+        System.out.println("data =" + data);
+    }
+
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+
+}
