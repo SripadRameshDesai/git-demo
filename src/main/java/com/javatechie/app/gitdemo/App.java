c536c6f98e669f7be42b170131b7b752aa0ab4e3
SripadRameshDesai <shripadrameshdesai@gmail.com>
2021-08-30 17:13:18 +0530

    added constant class and change in App.java

--- src/main/java/com/javatechie/app/gitdemo/App.java
+++ src/main/java/com/javatechie/app/gitdemo/App.java
@@ -1,11 +1,13 @@
 package com.javatechie.app.gitdemo;
 
+import static com.javatechie.app.gitdemo.Constant.DATA_TYPE;
+
 public class App {
 
     public void addData(String data){
-        System.out.println("data =" + data);
+        //System.out.println("data =" + data);
 
-        if(data.equals("java techie")){
+        if(data.equals(DATA_TYPE)){
             System.out.println("Welcome!!");
         }
     }
---
+++ src/main/java/com/javatechie/app/gitdemo/Constant.java
@@ -0,0 +1,5 @@
+package com.javatechie.app.gitdemo;
+
+public class Constant {
+    public static String DATA_TYPE = "Java Techie";
+}
